b8deeb333c3d4321d22a34ce807e7c43bdb74589
Kephren NZE <kephrennze@gmail.com>
2024-10-28 12:05:25 +0000

    first commit

---
+++ src/app/components/header.js
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header-item"></div>
+            <div className="header-item"></div>
+        </div>
+    );
+}
